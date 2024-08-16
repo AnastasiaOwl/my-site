b8c15fb2fd1fecfc249a424f836ed165fde19c8f
Filina Anastasia <124703588+AnastasiaOwl@users.noreply.github.com>
2024-08-16 20:04:53 +0300

    animated first intro page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [neonActive, setNeonActive] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setNeonActive(true);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className={`intro-image ${neonActive ? 'neon-active' : ''}`}>
+        <div classname='intro-text'>JavaScripte Developer</div>
+       </div>
     </div>
   );
 }
