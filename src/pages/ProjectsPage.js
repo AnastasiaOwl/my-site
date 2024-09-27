0ce3357c8927b3d33763e5be181c58a2388cd820
Filina Anastasia <124703588+AnastasiaOwl@users.noreply.github.com>
2024-09-27 12:00:21 +0300

    change projects and update CV

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -8,7 +8,7 @@ import useSound from 'use-sound';
 import clickSound from '../sounds/click.mp3'; 
 import rentImage from '../background-images/rent-photo/rent-main.png';
 import todoImage from '../background-images/todo-photo/todo-main.png';
-import dormitoryImage from '../background-images/dormitory-photo/dormitory-main.png'
+import gameImage from '../background-images/game-photo/game-main.png'
 import rentImage1 from '../background-images/rent-photo/rent1.png';
 import rentImage2 from '../background-images/rent-photo/rent2.png';
 import rentImage3 from '../background-images/rent-photo/rent3.png';
@@ -17,10 +17,9 @@ import todoImage1 from '../background-images/todo-photo/todo1.png';
 import todoImage2 from '../background-images/todo-photo/todo2.png';
 import todoImage3 from '../background-images/todo-photo/todo3.png';
 import todoImage4 from '../background-images/todo-photo/todo4.png';
-import dormitoryImage1 from '../background-images/dormitory-photo/dormitory1.png';
-import dormitoryImage2 from '../background-images/dormitory-photo/dormitory2.png';
-import dormitoryImage3 from '../background-images/dormitory-photo/dormitory3.png';
-import dormitoryImage4 from '../background-images/dormitory-photo/dormitory4.png';
+import gameImage1 from '../background-images/game-photo/game1.png';
+import gameImage2 from '../background-images/game-photo/game2.png';
+import gameImage3 from '../background-images/game-photo/game3.png';
 
 import '../styles/projects.css'
 import '../styles/App.css'
@@ -36,7 +35,7 @@ function ProjectsPage(){
 
     const rentImages = [rentImage, rentImage1, rentImage2, rentImage3, rentImage4];
     const todoImages = [todoImage, todoImage1, todoImage2, todoImage3, todoImage4];
-    const dormitoryImages = [dormitoryImage, dormitoryImage1, dormitoryImage2, dormitoryImage3, dormitoryImage4];
+    const gameImages = [gameImage, gameImage1, gameImage2, gameImage3];
     
     const openMap = () => {
     setShowMap(true); 
@@ -104,18 +103,17 @@ const goToPrevImage = () => {
                         offering features such as creation, management, and deletion of todo items.</div>
                     <div className="todoTools">Technologies Used: JavaScript, css, React</div>
                 </div>
-                <div className="projectDormitory">
-                    <div className="projectName"> Dormitory registration </div>
+                <div className="projectGame">
+                    <div className="projectName"> Halloween party </div>
                     <div 
-                    className="projectImage dormitory"
+                    className="projectImage game"
                     onMouseEnter={play}
-                    style={{ backgroundImage: `url(${dormitoryImage})`}}
-                    onClick={() => openModal(dormitoryImages, 0)}
+                    style={{ backgroundImage: `url(${gameImage})`}}
+                    onClick={() => openModal(gameImages, 0)}
                     ></div>
-                    <div className="dormitoryText">This project is a comprehensive Student Dormitory
-                        Management System designed to streamline operations for
-                        administrators, accountants, and dormitory staff.</div>
-                    <div className="dormitoryTools">Technologies Used: Java, Git, html/css, Spring, Mariadb database</div>
+                    <div className="gameText">This is fun and interactive Halloween-themed game
+                    where you must match icons on the grid and earn points.</div>
+                    <div className="gameTools">Technologies Used: JavaScript, css, React</div>
                 </div>
             </div>
            </div>
